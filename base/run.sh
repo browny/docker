ddb7d62f3c5cbacb353bc0877b1bb7c3ae5d12e8
Browny Lin <shlin1983@gmail.com>
2014-06-09 12:05:52 +0800

    Change ssh port

--- base/run.sh
+++ base/run.sh
@@ -4,4 +4,4 @@
 sh -c "docker build --rm -t base ."
 
 # Run a docker container with the image
-sh -c "docker run -t -i -p 8022:22 base"
+sh -c "docker run -t -i -p 2022:22 base"
--- mongodb/run.sh
+++ mongodb/run.sh
@@ -4,4 +4,4 @@
 sh -c "docker build --rm -t mongodb ."
 
 # Run a docker container with the image
-sh -c "docker run -t -i -p 23:22 -p 27017:27017 mongodb"
+sh -c "docker run -t -i -p 2022:22 -p 27017:27017 mongodb"
