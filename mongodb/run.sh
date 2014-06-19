5c755a3c69eee483789f5a37da051b5c017315ef
Browny Lin <shlin1983@gmail.com>
2014-06-19 14:52:49 +0000

    Keep data sync

--- mongodb/run.sh
+++ mongodb/run.sh
@@ -7,5 +7,5 @@ docker rm mongodb
 sh -c "docker build --rm -t browny/mongodb ."
 
 # Run a docker container with the image
-sh -c "docker run -d -t -i --name mongodb browny/mongodb"
-#sh -c "docker run -d -t -i -p 27017:27017 -v $(pwd)/db:/data/db --name mongodb browny/mongodb"
+#sh -c "docker run -d -t -i --name mongodb browny/mongodb"
+sh -c "docker run -t -i -p 23:22 -p 27017:27017 -v /root/docker/mongodb/db:/usr/local/mongodb-data --name mongodb browny/mongodb"
