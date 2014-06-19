9ed23498169847bd3b0de886ce6e3881ad219447
Browny Lin <shlin1983@gmail.com>
2014-06-19 14:15:35 +0800

    Give container the name

--- mongodb/run.sh
+++ mongodb/run.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+docker stop mongodb
+docker rm mongodb
+
 # Build docker image
-sh -c "docker build --rm -t mongodb ."
+sh -c "docker build --rm -t browny/mongodb ."
 
 # Run a docker container with the image
-sh -c "docker run -t -i -p 23:22 -p 27017:27017 mongodb"
+sh -c "docker run -d -t -i --name mongodb browny/mongodb"
+#sh -c "docker run -d -t -i -p 27017:27017 -v $(pwd)/db:/data/db --name mongodb browny/mongodb"
